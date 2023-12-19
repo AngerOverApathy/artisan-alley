f5db2562272ca5b47fb1141c3395566c823896c4
AngerOverApathy <violasian@gmail.com>
2023-12-19 14:46:39 -0800

    created array of products

--- src/data.js
+++ src/data.js
@@ -1,7 +1,178 @@
-const data = {
+const data = { 
     products: [
-        {
-            name: ''
+      // Blush
+      {
+        name: "Rosy Glow Cheek Blush",
+        slug: "rosy-glow-cheek-blush",
+        category: "blush",
+        image: "/images/blush1.jpg",
+        price: 15.99,
+        countInStock: 20,
+        brand: "Glamour Touch",
+        rating: 4.5,
+        numReviews: 12,
+        description: "A natural pink tone blush for a radiant, healthy glow."
+      },
+      // Cleansers
+      {
+        name: "Purifying Foam Cleanser",
+        slug: "purifying-foam-cleanser",
+        category: "cleansers",
+        image: "/images/cleanser1.jpg",
+        price: 12.99,
+        countInStock: 35,
+        brand: "Pure Skin",
+        rating: 4.8,
+        numReviews: 20,
+        description: "Gentle foam cleanser that removes impurities and soothes skin."
+      },
+      {
+        name: "Deep Cleanse Gel",
+        slug: "deep-cleanse-gel",
+        category: "cleansers",
+        image: "/images/cleanser2.jpg",
+        price: 13.50,
+        countInStock: 30,
+        brand: "Clean & Clear",
+        rating: 4.6,
+        numReviews: 18,
+        description: "Deep cleansing gel for removing oil and dirt, ideal for oily skin."
+      },
+      // Concealers
+      {
+        name: "Perfect Match Concealer",
+        slug: "perfect-match-concealer",
+        category: "concealers",
+        image: "/images/concealer1.jpg",
+        price: 10.99,
+        countInStock: 40,
+        brand: "Face Magic",
+        rating: 4.7,
+        numReviews: 25,
+        description: "Concealer that blends seamlessly with your skin tone."
+      },
+      {
+        name: "Full Coverage Concealer",
+        slug: "full-coverage-concealer",
+        category: "concealers",
+        image: "/images/concealer2.jpg",
+        price: 11.50,
+        countInStock: 38,
+        brand: "CoverAll",
+        rating: 4.4,
+        numReviews: 15,
+        description: "High coverage concealer for blemishes and dark circles."
+      },
+      // Lipsticks
+      {
+        name: "Velvet Matte Lipstick - Red",
+        slug: "velvet-matte-lipstick-red",
+        category: "lipsticks",
+        image: "/images/lipstick1.jpg",
+        price: 9.99,
+        countInStock: 50,
+        brand: "Lip Lux",
+        rating: 4.5,
+        numReviews: 30,
+        description: "Rich red matte lipstick for a bold, long-lasting look."
+      },
+      {
+        name: "Creamy Nude Lipstick",
+        slug: "creamy-nude-lipstick",
+        category: "lipsticks",
+        image: "/images/lipstick2.jpg",
+        price: 8.99,
+        countInStock: 45,
+        brand: "Natural Beauty",
+        rating: 4.3,
+        numReviews: 22,
+        description: "Soft nude shade with a creamy texture for everyday wear."
+      },
+      {
+        name: "Glossy Plum Lipstick",
+        slug: "glossy-plum-lipstick",
+        category: "lipsticks",
+        image: "/images/lipstick3.jpg",
+        price: 10.50,
+        countInStock: 42,
+        brand: "Gloss Goddess",
+        rating: 4.6,
+        numReviews: 28,
+        description: "Vibrant plum shade with a glossy finish for a stunning look."
+      },
+      // Eyeshadow Palettes
+      {
+        name: "Sunset Glow Eyeshadow Palette",
+        slug: "sunset-glow-eyeshadow-palette",
+        category: "eyeshadow-palettes",
+        image: "/images/eyeshadow1.jpg",
+        price: 20.99,
+        countInStock: 25,
+        brand: "Color Burst",
+        rating: 4.7,
+        numReviews: 17,
+        description: "Palette of warm shades inspired by sunset colors."
+      },
+      {
+        name: "Ocean Breeze Eyeshadow Palette",
+        slug: "ocean-breeze-eyeshadow-palette",
+        category: "eyeshadow-palettes",
+        image: "/images/eyeshadow2.jpg",
+        price: 21.50,
+        countInStock: 22,
+        brand: "Aqua Shades",
+        rating: 4.5,
+        numReviews: 19,
+        description: "Cool-toned palette reflecting the colors of the ocean."
+      },
+      // Serums
+      {
+        name: "Vitamin C Brightening Serum",
+        slug: "vitamin-c-brightening-serum",
+        category: "serums",
+        image: "/images/serum1.jpg",
+        price: 25.99,
+        countInStock: 30,
+        brand: "Skin Radiance",
+        rating: 4.9,
+        numReviews: 35,
+        description: "Brightening serum enriched with Vitamin C for glowing skin."
+      },
+      {
+        name: "Hyaluronic Hydration Serum",
+        slug: "hyaluronic-hydration-serum",
+        category: "serums",
+        image: "/images/serum2.jpg",
+        price: 24.99,
+        countInStock: 28,
+        brand: "Hydra Boost",
+        rating: 4.8,
+        numReviews: 32,
+        description: "Deep hydration serum with hyaluronic acid for all skin types."
+      },
+      // Shampoo Bars
+      {
+        name: "Lavender Essence Shampoo Bar",
+        slug: "lavender-essence-shampoo-bar",
+        category: "shampoo-bars",
+        image: "/images/shampoo1.jpg",
+        price: 7.99,
+        countInStock: 50,
+        brand: "Eco Hair",
+        rating: 4.6,
+        numReviews: 40,
+        description: "Soothing lavender shampoo bar for all hair types."
+      },
+      {
+        name: "Citrus Fresh Shampoo Bar",
+        slug: "citrus-fresh-shampoo-bar",
+        category: "shampoo-bars",
+        image: "/images/shampoo2.jpg",
+        price: 8.50,
+        countInStock: 45,
+        brand: "Fresh Start",
+        rating: 4.7,
+        numReviews: 38,
+        description: "Refreshing citrus shampoo bar for all hair types."
         }
-    ]
-}
+    ]}
