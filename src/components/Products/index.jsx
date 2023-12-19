eb462ccc3c4bd4c6b1dad621aa9ca65d916f10f3
AngerOverApathy <violasian@gmail.com>
2023-12-19 15:23:20 -0800

    fixed styling

--- src/components/Products/index.jsx
+++ src/components/Products/index.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
 import data from '../../data';
+import './style.css';
 
 export default function Products() {
     return (
         <>
         <main> 
             <h1>Featured Products</h1>
-            {data.products.map(product => (
-                <div>
-                    <img src={product.image} alt={product.name} />
-                    <h3>{product.name}</h3>
-                    <p>{product.description}</p>
-                    <p>{product.price}</p>
-                    <button>Add to Cart</button>
+                <div className='products'>
+                    {data.products.map(product => (
+                        <div className='product' key={product.slug}>
+                            <img src={product.image} alt={product.name} />
+                            <h3>{product.name}</h3>
+                            <p>{product.description}</p>
+                            <p>{product.price}</p>
+                            <button>Add to Cart</button>
+                        </div>
+                    ))}
                 </div>
-            ))}
         </main>
         </>
     );
