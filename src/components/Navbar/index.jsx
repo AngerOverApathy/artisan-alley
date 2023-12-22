629c7d4e61fe8cc6562debd157e9f18cce19aeb6
AngerOverApathy <violasian@gmail.com>
2023-12-22 15:11:13 -0800

    added route

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -3,17 +3,17 @@ import './style.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomeScreen from '../HomeScreen'
 
-export default function Header () {
-  return (
-    <BrowserRouter>
-    <>
-        <header>
-            <a href='/'>Elegant Essence</a>
-        </header>
-        <Routes>
-          <Route path='/' element={<HomeScreen />} />
-        </Routes>        
-    </>
-    </BrowserRouter>
-  )
-}
+// export default function Header () {
+//   return (
+//     <BrowserRouter>
+//     <>
+//         <header>
+//             <a href='/'>Elegant Essence</a>
+//         </header>
+//         <Routes>
+//           <Route path='/' element={<HomeScreen />} />
+//         </Routes>        
+//     </>
+//     </BrowserRouter>
+//   )
+// }
