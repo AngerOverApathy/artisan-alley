190b822f4b525695c706b72cc07ea8e49d47fc5d
AngerOverApathy <violasian@gmail.com>
2023-12-23 15:58:09 -0800

    fixed a tag

--- src/components/HomeScreen/index.jsx
+++ src/components/HomeScreen/index.jsx
@@ -1,27 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import data from '../../data';
 import './style.css';
 
 export default function HomeScreen() {
     return (
-            <>
+        <>
             <main>
                 <h1>Featured Products</h1>
-                    <div className='products'>
-                        {data.products.map(product => (
-                            <div className='product' key={product.slug}>
-                                <img src={product.image} alt={product.name} />
-                                <div className='product-info'>
-                                <a href={`/product/${product.slug}`}>
+                <div className='products'>
+                    {data.products.map(product => (
+                        <div className='product' key={product.slug}>
+                            <img src={product.image} alt={product.name} />
+                            <div className='product-info'>
+                                <Link to={`/product/${product.slug}`}>
                                     <h3>{product.name}</h3>
-                                </a>
-                                    <p><strong>${product.price}</strong></p>
-                                    <button>Add to Cart</button>
-                                </div>
+                                </Link>
+                                <p><strong>${product.price.toFixed(2)}</strong></p>
+                                <button>Add to Cart</button>
                             </div>
-                        ))}
-                    </div>
+                        </div>
+                    ))}
+                </div>
             </main>
-            </>
+        </>
     );
 }
--- src/components/ProductScreen/index.jsx
+++ src/components/ProductScreen/index.jsx
@@ -1,3 +1,12 @@
+import React from 'react'
+import { useParams } from 'react-router-dom';
+
 export default function ProductScreen() {
-    return <div>Product Screen</div>
+    const params = useParams();
+    const { slug } = params;
+    return (
+        <div>
+            <h1>{slug}</h1>
+        </div>
+    )
 }
