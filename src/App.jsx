08a26ccc928f2599b91d36bd4925754f2b18783c
AngerOverApathy <violasian@gmail.com>
2023-12-23 15:29:11 -0800

    modified components

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
-import Navbar from './components/Navbar'
-import HomeScreen from './components/HomeScreen'
-import './App.css'
+import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import HomeScreen from './components/HomeScreen';
+import ProductScreen from './components/ProductScreen';
+import './App.css';
 
 function App() {
-
   return (
-    <>
-      <Navbar />
-      <HomeScreen />
-    
-    </>
-  )
+    <BrowserRouter>
+      <>
+        <header>
+          <Link to='/'>Elegant Essence</Link>
+        </header>
+        <Routes>
+          <Route path='/product/:slug' element={<ProductScreen />} />
+          <Route path='/' element={<HomeScreen />} />
+        </Routes>
+      </>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
